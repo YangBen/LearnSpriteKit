cd7e255ffb2d790ddc438990e08cc0796010e76c
Ben <ben@BendeMac-mini.local>
2015-01-08 19:04:55 +0800

    v1.0.2 new project, version num auto add.

--- 20150107-ZombieConga/GameScene.swift
+++ 20150107-ZombieConga/GameScene.swift
@@ -9,37 +9,34 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+    var _zombie = SKSpriteNode(imageNamed: "zombie")
+    
+    override func update(currentTime: NSTimeInterval) {
         
-        self.addChild(myLabel)
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+    override func didMoveToView(view: SKView){
+        // set background
+        backgroundColor = SKColor.whiteColor()
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        // set background img
+        let background = SKSpriteNode(imageNamed: "background")
+        let mysize = background.size
+        println("Size: \(mysize)")
+        
+        background.size = size
+        background.zPosition = -1
+        addChild(background)
+        
+        // set background img path
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        
+        //------------------------------------------------------------
+        
+        // add zombie
+        _zombie.size = CGSize(width: 340, height: 260)
+        _zombie.setScale(2.0)
+        _zombie.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(_zombie)
     }
 }
